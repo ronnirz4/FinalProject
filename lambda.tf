d4e2c295a77b8c036aaa0e13668f5b133221ef88
ronel <ronel1354@gmail.com>
2024-12-01 20:15:24 +0200

    update iam and lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -1,11 +1,44 @@
-# Lambda Function for Code Validation
 resource "aws_lambda_function" "validate_code" {
   function_name    = "Ronn4ValidateCode"  # Name of the Lambda function
   role             = aws_iam_role.lambda_execution_role.arn  # Reference the role from iam.tf
   handler          = "validate_code.lambda_handler"  # Entry point of the Lambda function
   runtime          = "python3.9"  # Python runtime version
-  filename         = "validate_code.zip"  # The name of the ZIP file containing the code
-  source_code_hash = filebase64sha256("validate_code.zip")  # Hash of the ZIP file
+
+  # Specify the S3 bucket and key for the Lambda code
+  s3_bucket        = "ronn4finaproject"  # The name of the S3 bucket
+  s3_key           = "Ronn4ValidateCode"      # The path to the .zip file in the S3 bucket
 }
 
-# If you need additional Lambda functions like DeployToStaging, RunTests, etc., repeat the above Lambda function block.
+resource "aws_lambda_function" "deploy_to_staging" {
+  function_name = "Ronn4DeployToStaging"
+  role          = aws_iam_role.lambda_execution_role.arn
+  handler       = "index.lambda_handler"
+  runtime       = "python3.9"
+
+  # Specify the S3 bucket and key for the Lambda code
+  s3_bucket     = "ronn4-lambda-bucket"  # The name of the S3 bucket
+  s3_key        = "staging_lambda_code.zip"  # The path to the .zip file in the S3 bucket
+
+  environment {
+    variables = {
+      DEPLOYMENT_BUCKET = "ronn4-staging-bucket"
+    }
+  }
+}
+
+resource "aws_lambda_function" "deploy_to_production" {
+  function_name = "Ronn4DeployToProduction"
+  role          = aws_iam_role.lambda_execution_role.arn
+  handler       = "index.lambda_handler"
+  runtime       = "python3.9"
+
+  # Specify the S3 bucket and key for the Lambda code
+  s3_bucket     = "ronn4-lambda-bucket"  # The name of the S3 bucket
+  s3_key        = "production_lambda_code.zip"  # The path to the .zip file in the S3 bucket
+
+  environment {
+    variables = {
+      DEPLOYMENT_BUCKET = "ronn4-production-bucket"
+    }
+  }
+}
