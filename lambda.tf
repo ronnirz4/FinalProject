123e64fb4f4f3a96aecef5b7026fea95755e1c67
ronel <ronel1354@gmail.com>
2024-12-04 19:33:17 +0200

    update files

--- lambda.tf
+++ lambda.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = "us-east-2"  # Replace with your desired region
-}
-
 resource "aws_lambda_function" "Ronn4ValidateCode" {
   function_name = "Ronn4ValidateCode"
   role          = aws_iam_role.lambda_execution_role.arn
@@ -41,44 +37,4 @@ resource "aws_lambda_function" "Ronn4DeployToProduction" {
   timeout       = 300
 
   source_code_hash = ""  # Empty because code is uploaded manually
-}
-
-# IAM Role for Lambda execution (same as before)
-resource "aws_iam_role" "lambda_execution_role" {
-  name = "lambda_execution_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-# Attach the necessary permissions to the IAM role (same as before)
-resource "aws_iam_role_policy" "lambda_policy" {
-  name = "lambda_policy"
-  role = aws_iam_role.lambda_execution_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "logs:*",
-          "s3:*",  # Example of a service the Lambda might interact with
-          "codecommit:GitPull",  # Example of another AWS service the Lambda might need access to
-          "ecs:DescribeServices"
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      }
-    ]
-  })
 }
