8d8180f171ca8baf1479b4b65a44c2c75236ed96
ronel <ronel1354@gmail.com>
2024-12-04 19:34:36 +0200

    update files

--- step_function.tf
+++ step_function.tf
@@ -1,55 +1,62 @@
-definition = jsonencode({
-    Comment: "Pipeline for Serverless Application Deployment",
-    StartAt: "ValidateCode",
-    States: {
-      ValidateCode: {
-        Type: "Task",
-        Resource: "arn:aws:lambda:us-east-2:023196572641:function:Ronn4ValidateCode",  # Replace with your Lambda ARN
-        Next: "DeployToStaging",
-        Catch: [
-          {
-            ErrorEquals: ["States.ALL"],
-            Next: "Rollback"
-          }
-        ]
-      },
-      DeployToStaging: {
-        Type: "Task",
-        Resource: "arn:aws:lambda:us-east-2:023196572641:function:Ronn4DeployToStaging",  # Replace with your Lambda ARN
-        Next: "RunTests",
-        Catch: [
-          {
-            ErrorEquals: ["States.ALL"],
-            Next: "Rollback"
-          }
-        ]
-      },
-      RunTests: {
-        Type: "Task",
-        Resource: "arn:aws:lambda:us-east-2:023196572641:function:Ronn4RunTests",  # Replace with your Lambda ARN
-        Next: "DeployToProduction",
-        Catch: [
-          {
-            ErrorEquals: ["States.ALL"],
-            Next: "Rollback"
-          }
-        ]
-      },
-      DeployToProduction: {
-        Type: "Task",
-        Resource: "arn:aws:lambda:us-east-2:023196572641:function:Ronn4DeployToProduction",  # Replace with your Lambda ARN
-        End: true,
-        Catch: [
-          {
-            ErrorEquals: ["States.ALL"],
-            Next: "Rollback"
-          }
-        ]
-      },
-      Rollback: {
-        Type: "Task",
-        Resource: "arn:aws:lambda:us-east-2:023196572641:function:Ronn4RollbackDeployment",  # Replace with your Lambda ARN
-        End: true
-      }
+resource "aws_sfn_state_machine" "Ronn4DeploymentPipeline" {
+  name     = "Ronn4DeploymentPipeline"
+  role_arn = aws_iam_role.lambda_execution_role.arn  # Role for Step Function to execute Lambda
+
+  definition = <<EOF
+{
+  "Comment": "Pipeline for Serverless Application Deployment",
+  "StartAt": "ValidateCode",
+  "States": {
+    "ValidateCode": {
+      "Type": "Task",
+      "Resource": "arn:aws:lambda:us-east-2:023196572641:function:Ronn4ValidateCode",  # Replace with your Lambda ARN
+      "Next": "DeployToStaging",
+      "Catch": [
+        {
+          "ErrorEquals": ["States.ALL"],
+          "Next": "Rollback"
+        }
+      ]
+    },
+    "DeployToStaging": {
+      "Type": "Task",
+      "Resource": "arn:aws:lambda:us-east-2:023196572641:function:Ronn4DeployToStaging",  # Replace with your Lambda ARN
+      "Next": "RunTests",
+      "Catch": [
+        {
+          "ErrorEquals": ["States.ALL"],
+          "Next": "Rollback"
+        }
+      ]
+    },
+    "RunTests": {
+      "Type": "Task",
+      "Resource": "arn:aws:lambda:us-east-2:023196572641:function:Ronn4RunTests",  # Replace with your Lambda ARN
+      "Next": "DeployToProduction",
+      "Catch": [
+        {
+          "ErrorEquals": ["States.ALL"],
+          "Next": "Rollback"
+        }
+      ]
+    },
+    "DeployToProduction": {
+      "Type": "Task",
+      "Resource": "arn:aws:lambda:us-east-2:023196572641:function:Ronn4DeployToProduction",  # Replace with your Lambda ARN
+      "End": true,
+      "Catch": [
+        {
+          "ErrorEquals": ["States.ALL"],
+          "Next": "Rollback"
+        }
+      ]
+    },
+    "Rollback": {
+      "Type": "Task",
+      "Resource": "arn:aws:lambda:us-east-2:023196572641:function:Ronn4RollbackDeployment",  # Replace with your Lambda ARN
+      "End": true
     }
-})
+  }
+}
+EOF
+}
